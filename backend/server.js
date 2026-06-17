8580b60a28a4afdf877b3f5861b344609be967ed
Igor <dyakov-igor999@mail.ru>
2026-06-17 21:38:58 +0500

    final

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '..')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
 const SECRET_KEY = '8bgn_clPRLksiZ-yoKnCAbCzXhxMDg';
 
 // Подключение к SQLite
