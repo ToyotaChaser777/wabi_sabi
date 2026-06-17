2a7cb82360f159504ed0d9a080f6e8d6a2105dea
Igor <dyakov-igor999@mail.ru>
2026-06-17 21:19:08 +0500

    final

--- backend/server.js
+++ backend/server.js
@@ -224,7 +224,7 @@ app.get('/api/orders/:user_id', (req, res) => {
     );
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`🚀 Сервер запущен на http://localhost:${PORT}`);
+    console.log(`🚀 Сервер запущен на порту ${PORT}`);
 });
